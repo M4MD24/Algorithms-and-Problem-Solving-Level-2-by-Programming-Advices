115cf1e725a35cfb87c88a170f9bd07321d69294
m4md24 <m4md24@gmail.com>
2025-04-23 11:17:10 +0200

    # Solve Problem 20

---
+++ src/_2_problems_from_11_to_20/_2_10_problem_20/Problem20.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+short randomNumber(const short FROM, const short TO) { return rand() % (TO - FROM + 1) + FROM; }
+
+enum CharacterType {
+    SmallLetter = 1,
+    CapitalLetter = 2,
+    SpecialLetter = 3,
+    Digit = 4
+};
+
+char randomCharacter(const CharacterType CHARACTER_TYPE) {
+    switch (CHARACTER_TYPE) {
+    case SmallLetter:
+        return static_cast<char>(randomNumber('a', 'z'));
+    case CapitalLetter:
+        return static_cast<char>(randomNumber('A', 'Z'));
+    case SpecialLetter:
+        return static_cast<char>(randomNumber('!', '/'));
+    case Digit:
+        return static_cast<char>(randomNumber('0', '9'));
+    }
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    cout << randomCharacter(SmallLetter) << endl;
+    cout << randomCharacter(CapitalLetter) << endl;
+    cout << randomCharacter(SpecialLetter) << endl;
+    cout << randomCharacter(Digit) << endl;
+}
